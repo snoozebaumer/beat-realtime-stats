42ecc7ea4dd701efbe0b29feef16019fc6b1feaf
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2022-10-21 12:21:10 +0200

    #BEAT-5: set up basic nodejs implementation example with typescript

---
+++ Backend/server.ts
@@ -0,0 +1,17 @@
+import {Request,Response,Application} from 'express';
+import express = require('express');
+import {Attempt} from "./attempt";
+
+const PORT = 3000;
+
+const APP: Application = express();
+
+APP.get("/", (req:Request, res:Response):void => {
+    let attempt: Attempt = new Attempt();
+    let time = attempt.getTime();
+    res.send("Hello world! Time elapsed: " + time.minutes + " Minutes " + time.seconds + " seconds");
+});
+
+APP.listen(PORT, ():void => {
+    console.log(`Server Running here -> http://localhost:${PORT}`);
+});
