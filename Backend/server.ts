0a8c98c93145baf69d95254fe367658fd55e9d94
Dave Decker <david.decker@stud.hslu.ch>
2022-10-27 10:40:28 +0200

    fix import for server

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,17 +1,23 @@
-import {Request,Response,Application} from 'express';
-import express = require('express');
-import {Attempt} from "./attempt";
+import { Request, Response, Application } from "express";
+import express from "express";
+import { Attempt } from "./attempt";
 
 const PORT = 3000;
 
 const APP: Application = express();
 
-APP.get("/", (req:Request, res:Response):void => {
-    let attempt: Attempt = new Attempt();
-    let time = attempt.getTime();
-    res.send("Hello world! Time elapsed: " + time.minutes + " Minutes " + time.seconds + " seconds");
+APP.get("/", (req: Request, res: Response): void => {
+  let attempt: Attempt = new Attempt();
+  let time = attempt.getTime();
+  res.send(
+    "Hello world! Time elapsed: " +
+      time.minutes +
+      " Minutes " +
+      time.seconds +
+      " seconds"
+  );
 });
 
-APP.listen(PORT, ():void => {
-    console.log(`Server Running here -> http://localhost:${PORT}`);
+APP.listen(PORT, (): void => {
+  console.log(`Server Running here -> http://localhost:${PORT}`);
 });
