d09820fc35a138d3e7da4d45c8e6fcc231b06aeb
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2022-11-11 12:04:41 +0100

    changed backend to deliver frontend on root

--- Backend/server.ts
+++ Backend/server.ts
@@ -2,12 +2,19 @@ import {Request, Response, Application} from "express";
 // @ts-ignore
 import express from "express";
 import {Run} from "./run";
+import * as path from "path";
 
 const PORT = 3000;
 
 const APP: Application = express();
 
 let run: Run;
+
+APP.use(express.static(path.resolve('../frontend')));
+
+APP.get('/', (req: Request, res: Response): void => {
+    res.sendFile(path.resolve('../frontend/index.html'))
+});
 
 APP.get("/start", (req: Request, res: Response): void => {
     if (run && run.isRunning) {
@@ -19,7 +26,7 @@ APP.get("/start", (req: Request, res: Response): void => {
 
     try {
         run = new Run();
-    } catch (err) {
+    } catch (err: any) {
         res.status(500).json({
             message: err.message
         });
