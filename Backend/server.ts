594f8123c878ac7a091ec15686db34e9087b2651
Dave Decker <david.decker@stud.hslu.ch>
2023-03-10 11:35:20 +0100

    add websocket to update client from server

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,97 +1,118 @@
 // @ts-ignore
-import express, {Application, Request, Response} from "express";
-import {GarbageType, Run} from "./run";
+import express, { Application, json, Request, Response } from "express";
+import * as http from "http";
 import * as path from "path";
+import * as WebSocket from "ws";
+import { GarbageType, Run } from "./run";
 
 const PORT = 3000;
 
 const APP: Application = express();
 
+const server = http.createServer(APP);
+
+const wss = new WebSocket.Server({ server });
+
 let run: Run;
 
-APP.use(express.static(path.resolve('../frontend')));
+function updateClients() {
+  wss.clients.forEach((client) =>
+    client.send(JSON.stringify(run.getClientData()))
+  );
+}
 
-APP.get('/', (req: Request, res: Response): void => {
-    res.sendFile(path.resolve('../frontend/index.html'))
+wss.on("connection", (ws: WebSocket) => {
+  if (run && run.isRunning) {
+    ws.send(JSON.stringify(run.getClientData()));
+  }
+});
+
+APP.use(express.static(path.resolve("../frontend")));
+
+APP.get("/", (req: Request, res: Response): void => {
+  res.sendFile(path.resolve("../frontend/index.html"));
 });
 
 APP.get("/start", (req: Request, res: Response): void => {
-    if (run && run.isRunning) {
-        res.status(409).json({
-            message: "Run has already been started"
-        });
-        return;
-    }
+  if (run && run.isRunning) {
+    res.status(409).json({
+      message: "Run has already been started",
+    });
+    return;
+  }
 
-    try {
-        run = new Run();
-    } catch (err: any) {
-        res.status(500).json({
-            message: err.message
-        });
-    }
+  try {
+    run = new Run();
+  } catch (err: any) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 
-    run.save().then((success) => {
-        if (success) {
-            res.status(200).json({
-                message: "Run " + run.id + " started"
-            });
-        } else {
-            res.status(500).json({
-                message: "Something went wrong"
-            });
-        }
-    })
+  run.save().then((success) => {
+    if (success) {
+      updateClients();
+      res.status(200).json({
+        message: "Run " + run.id + " started",
+      });
+    } else {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  });
 });
 
 APP.get("/stop", (req: Request, res: Response): void => {
-    if (!run || !run.isRunning) {
-        res.status(409).json({
-            message: "Run hasn't been started"
-        });
-        return;
-    }
-
-    run.isRunning = false;
-    run.endDateTime = new Date();
-
-    run.save().then((success) => {
-        if (success) {
-            res.status(200).json({
-                message: "Run " + run.id + " stopped"
-            });
-        } else {
-            res.status(500).json({
-                message: "Something went wrong"
-            });
-        }
+  if (!run || !run.isRunning) {
+    res.status(409).json({
+      message: "Run hasn't been started",
     });
+    return;
+  }
+
+  run.isRunning = false;
+  run.endDateTime = new Date();
+
+  run.save().then((success) => {
+    if (success) {
+      updateClients();
+      res.status(200).json({
+        message: "Run " + run.id + " stopped",
+      });
+    } else {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  });
 });
 
 APP.get("/foundObject/:id", (req: Request, res: Response): void => {
-    if (!run || !run.isRunning) {
-        res.status(409).json({
-            message: "Run hasn't been started"
-        });
-        return;
-    }
-
-    let garbage: GarbageType = parseInt(req.params.id);
-    run.addGarbage(garbage);
-
-    run.save().then((success) => {
-        if (success) {
-            res.status(200).json({
-                message: "Garbage added"
-            });
-        } else {
-            res.status(500).json({
-                message: "Something went wrong"
-            });
-        }
+  if (!run || !run.isRunning) {
+    res.status(409).json({
+      message: "Run hasn't been started",
     });
+    return;
+  }
+
+  let garbage: GarbageType = parseInt(req.params.id);
+  run.addGarbage(garbage);
+
+  run.save().then((success) => {
+    if (success) {
+      updateClients();
+      res.status(200).json({
+        message: "Garbage added",
+      });
+    } else {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  });
 });
 
-APP.listen(PORT, (): void => {
-    console.log(`Server Running here -> http://localhost:${PORT}`);
+server.listen(PORT, (): void => {
+  console.log(`Server Running here -> http://localhost:${PORT}`);
 });
--- frontend/vue.js
+++ frontend/vue.js
@@ -1,6 +1,42 @@
+const data = {
+    connection: null,
+    run: {
+        id: null,
+        startDateTime: null,
+        endDateTime: null,
+        currentRunTime: '-',
+        isRunning: false,
+        status: "Ausser Betrieb",
+        bottleCapAmount: 0,
+        cigaretteAmount: 0,
+        plasticCapAmount: 0,
+        keyAmount: 0,
+        coinAmount: 0,
+        ringAmount: 0,
+    },
+
+};
+
 var app = new Vue({
-    el: '#app',
-    data: {
-        status: 'In Betrieb'
-    }
-})
+    el: "#app",
+    data,
+    created: () => {
+        console.log("Connecting to server...");
+
+        data.connection = new WebSocket(`ws://${window.location.hostname}:3000`);
+
+        data.connection.onopen = (event) => {
+            console.log("Successfully connected to server");
+        };
+
+        data.connection.onmessage = (event) => {
+            const eventData = JSON.parse(event.data);
+            const newRun = {
+                ...data.run,
+                ...eventData,
+                status: eventData.isRunning ? "In Betrieb" : "Ausser Betrieb",
+            };
+            data.run = newRun;
+        };
+    },
+});
